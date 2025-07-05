c8f9bff6a6c72990b7b7730e0106c84e19c667a8
CodeMasterKR <ibrahimovkamronbek7@gmail.com>
2025-07-05 14:14:41 +0500

    sdfjdsf

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import { jwtDecode } from 'jwt-decode';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'http://api.londoneducation.uz';
 export const AUTH_TOKEN_KEY = 'authToken';
 
 export async function apiRequest(endpoint, method = 'GET', body = null, tokenParam = null, isFormData = false) {
